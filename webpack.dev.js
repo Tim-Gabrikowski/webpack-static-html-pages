19cb3da281f189e5cad56d83c95bb77afe81baf6
Tim Gabrikowski <timgabrikowski@outlook.de>
2023-04-14 19:35:54 +0200

    changes to config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,26 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
   // https://webpack.js.org/configuration/mode/
-  mode: 'development',
+  mode: "development",
 
   // This option controls if and how source maps are generated.
   // https://webpack.js.org/configuration/devtool/
-  devtool: 'eval-cheap-module-source-map',
+  devtool: "eval-cheap-module-source-map",
 
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
-    index: './src/page-index/main.js',
-    about: './src/page-about/main.js',
-    contacts: './src/page-contacts/main.js'
+    index: "./src/page-index/main.js",
+    about: "./src/page-about/main.js",
+    contacts: "./src/page-contacts/main.js",
   },
 
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    port: 8080,
-    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    port: 8085,
+    devMiddleware: {
+      writeToDisk: false, // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    },
   },
 
   // https://webpack.js.org/concepts/loaders/
@@ -30,54 +31,54 @@ module.exports = {
         test: /\.m?js$/i,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         // https://webpack.js.org/loaders/css-loader/#root
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         // https://webpack.js.org/guides/asset-modules/#resource-assets
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
       {
         // https://webpack.js.org/guides/asset-modules/#replacing-inline-loader-syntax
         resourceQuery: /raw/,
-        type: 'asset/source'
+        type: "asset/source",
       },
       {
         // https://webpack.js.org/loaders/html-loader/#usage
         resourceQuery: /template/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: "html-loader",
+      },
+    ],
   },
 
   // https://webpack.js.org/concepts/plugins/
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/page-index/tmpl.html',
+      template: "./src/page-index/tmpl.html",
       inject: true,
-      chunks: ['index'],
-      filename: 'index.html'
+      chunks: ["index"],
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-      template: './src/page-about/tmpl.html',
+      template: "./src/page-about/tmpl.html",
       inject: true,
-      chunks: ['about'],
-      filename: 'about.html'
+      chunks: ["about"],
+      filename: "about.html",
     }),
     new HtmlWebpackPlugin({
-      template: './src/page-contacts/tmpl.html',
+      template: "./src/page-contacts/tmpl.html",
       inject: true,
-      chunks: ['contacts'],
-      filename: 'contacts.html'
-    })
-  ]
-}
+      chunks: ["contacts"],
+      filename: "contacts.html",
+    }),
+  ],
+};
