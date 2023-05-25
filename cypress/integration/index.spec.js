c1e9e12e4958c208827d85ac8e3a75bec9f49b34
Tim Gabrikowski <tim@gabrikowski.de>
2023-05-25 15:09:18 +0200

    add package-json

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,4 +1,4 @@
-describe("ecample test", () => {
+describe("example test", () => {
 	context("example", () => {
 		it("can recognise that true is equal to true", () => {
 			expect(true).to.eq(true);
